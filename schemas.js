d34dea79d384069b1775ad34a5d75fded7d961a4
UNKNOWN-X <71239907+UNKNOWN-X0@users.noreply.github.com>
2025-12-10 21:27:23 +0100

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -2,7 +2,7 @@
 const schemas = {
     characters: {
         label: "Characters",
-        icon: "`
+        icon: `
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
                  fill="none" stroke="currentColor" stroke-width="2" 
                  stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
